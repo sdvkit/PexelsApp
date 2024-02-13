2cf87a422e391b908c6970e8b9d219fecbdc5add
sdvkit <me.sudaev.nikita@gmail.com>
2024-02-13 20:02:10 +0100

    Custom NotificationManager created

--- app/src/main/java/com/sdv/kit/pexelsapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/sdv/kit/pexelsapp/presentation/activity/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.sdv.kit.pexelsapp.presentation.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sdv.kit.pexelsapp.R
 import com.sdv.kit.pexelsapp.data.manager.NotificationManagerImpl
@@ -25,6 +30,8 @@ class MainActivity : ComponentActivity() {
             })
         }
 
+        handleNotificationsPermission()
+
         setContent {
             AppTheme {
                 NavGraph(startDestination = viewModel.startDestination.value)
@@ -32,6 +39,22 @@ class MainActivity : ComponentActivity() {
                 if (viewModel.fcmToken.value.isNotBlank()) {
                     sendFCMNotification()
                 }
+            }
+        }
+    }
+
+    private fun handleNotificationsPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    REQUEST_NOTIFICATION_PERMISSION_CODE
+                )
             }
         }
     }
@@ -48,5 +71,9 @@ class MainActivity : ComponentActivity() {
             pendingIntent = null,
             isHighPriority = true
         )
+    }
+
+    companion object {
+        private const val REQUEST_NOTIFICATION_PERMISSION_CODE = 101
     }
 }
