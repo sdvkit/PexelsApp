5b7de439a4334f5d3715eb04f6e681de04cc9278
sdvkit <me.sudaev.nikita@gmail.com>
2024-02-13 20:04:58 +0100

    ChargingWorker created

---
+++ app/src/main/java/com/sdv/kit/pexelsapp/presentation/worker/ChargingWorker.kt
@@ -0,0 +1,40 @@
+package com.sdv.kit.pexelsapp.presentation.worker
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.sdv.kit.pexelsapp.R
+import com.sdv.kit.pexelsapp.data.manager.NotificationManagerImpl
+
+class ChargingWorker(
+    appContext: Context,
+    params: WorkerParameters
+) : CoroutineWorker(appContext, params) {
+
+    override suspend fun doWork(): Result {
+        val notificationManager = NotificationManagerImpl(context = applicationContext)
+        val pendingIntent = buildPendingIntent()
+
+        notificationManager.sendNotification(
+            icon = R.drawable.ic_logo,
+            title = applicationContext.getString(R.string.charge_notification_title),
+            text = applicationContext.getString(R.string.charge_notification_text),
+            autoCancel = true,
+            pendingIntent = pendingIntent
+        )
+
+        return Result.success()
+    }
+
+    private fun buildPendingIntent(): PendingIntent {
+        val intent = Intent()
+        return PendingIntent.getActivity(
+            applicationContext,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+    }
+}
