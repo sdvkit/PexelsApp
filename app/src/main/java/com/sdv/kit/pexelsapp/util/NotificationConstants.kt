bb9c2b61cc09324fc91adb109629defe409cb2cf
sdvkit <me.sudaev.nikita@gmail.com>
2024-02-15 13:57:33 +0100

    Created NotificationConstants

---
+++ app/src/main/java/com/sdv/kit/pexelsapp/util/NotificationConstants.kt
@@ -0,0 +1,10 @@
+package com.sdv.kit.pexelsapp.util
+
+object NotificationConstants {
+
+    const val CHANNEL_ID = "pexels_channel_id"
+    const val CHANNEL_NAME = "pexels_channel_name"
+
+    const val NOTIFICATION_CHARGING_ID = 1
+    const val NOTIFICATION_DAILY_PHOTO_ID = 2
+}
